63ca934cfdacf9a4fab919b8573c5900e30d12ea
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-03-03 22:43:46 -0600

    prettified and added styles to nav

--- components/navBarButton.tsx
+++ components/navBarButton.tsx
@@ -1,15 +1,11 @@
-/** @jsxImportSource @emotion/react */
 import * as React from "react";
-import * as Button from "@mui/material/Button";
 import Link from "next/link";
-import { css } from '@emotion/react'
 
 const NavLinkButton = (props: any) => {
   return (
     <React.Fragment>
       <Link href={props.to} as={props.to} shallow>
-        <a
-    >{props.children}</a>
+        <a {...props.newTab && {target: "_blank", rel: "noreferrer"}}>{props.children}</a>
       </Link>
     </React.Fragment>
   );
