16e143b316ccf5d5b4f2273fe2693b1f3bf4596d
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-03-04 00:28:08 -0600

    added mobile sidebar branch

--- lib/components/ui/header.tsx
+++ lib/components/ui/header.tsx
@@ -5,6 +5,16 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import MenuIcon from "@mui/icons-material/Menu";
 import styled from "@emotion/styled";
 import NavLinkButton from "../navBarButton";
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 const navLinks: any = {
   Services: "/services",
@@ -65,6 +75,57 @@ const StyledNav = styled.nav`
 `;
 
 const Header = () => {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor: any, open:any) => (event:any) => {
+    console.log('opening')
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor: string) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  
   const createNavLinks = () => {
     return Object.keys(navLinks).map((item: string, index: number) => {
       return (
@@ -89,6 +150,10 @@ const Header = () => {
     });
   };
 
+  const test = () => {
+    console.log('test2')
+  }
+
   return (
     <React.Fragment>
       <BaseAppBar color="transparent" elevation={0} position="static">
@@ -105,11 +170,35 @@ const Header = () => {
             <Grid className="nav-col" item>
               {createSocialLinks()}
             </Grid>
+            <Button onClick={test}>test</Button>
             {
               <Grid className="menu-icon" item>
-                <NavLinkButton to="/">
+                 {/* <Button onClick={()=>toggleDrawer('left', true)}>{<MenuIcon />}</Button>
+                   <SwipeableDrawer
+                   anchor={'left'}
+                   open={state['left']}
+                   onClose={toggleDrawer('left', false)}
+                   onOpen={() => toggleDrawer('left', true)}
+                 >
+                   {list('left')}
+                 </SwipeableDrawer> */}
+              
+                  {['left', 'right', 'top', 'bottom'].map((anchor: string) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={() => toggleDrawer(anchor, true)}>{anchor}</Button>
+          <SwipeableDrawer
+            anchor="left"
+            open={state['left']}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+                {/* <NavLinkButton to="/">
                   <MenuIcon />
-                </NavLinkButton>
+                </NavLinkButton> */}
               </Grid>
             }
           </StyledNav>
