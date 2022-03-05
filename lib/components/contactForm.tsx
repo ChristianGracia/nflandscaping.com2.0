d6ec6ade5045ef70bc76cde9087e2aab29661cba
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-03-05 15:35:17 -0600

    created contact form

--- lib/components/contactForm.tsx
+++ lib/components/contactForm.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from '@mui/material/TextField';
+
 
 const ContactForm = (props: any)  => {
   return (
-    <React.Fragment>
-        <p>Form</p>
-    </React.Fragment>
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+      display="flex" flexDirection="column" alignItems="center" 
+    >
+      <TextField
+        id="name-input"
+        label="Name"
+        type="text"
+        autoComplete="name"
+        variant="standard"
+      />
+      <TextField
+        id="phone-input"
+        label="Phone Number"
+        type="text"
+        autoComplete="phone"
+        variant="standard"
+      />
+       <TextField
+        id="phone-input"
+        label="Message"
+        type="text"
+        variant="standard"
+      />
+  </Box>
   );
 };
 
