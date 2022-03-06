7faa51d8d442520af3dc2620b605200b7f992437
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-03-06 01:57:51 -0600

    styled carousel

--- lib/components/carouselSlide.tsx
+++ lib/components/carouselSlide.tsx
@@ -7,6 +7,7 @@ import Box from "@mui/material/Box";
 interface Item {
   image: string;
   description: string;
+  title: string;
 }
 interface CarouselSlideProps {
   item: Item;
@@ -15,8 +16,15 @@ interface CarouselSlideProps {
 const CarouselSlide = ({ item }: CarouselSlideProps) => {
   const StyledContainer = styled.div`
     width: 100%;
-    height: 100%;
-    background-image: url(/images/carousel-images/${item.image});
+    height: calc(100vh - 114px);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url(/images/carousel-images/${item.image});
+    background-position: center;
+    background-size: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
     .button-div {
       display: flex;
       flex-direction: row;
@@ -25,13 +33,48 @@ const CarouselSlide = ({ item }: CarouselSlideProps) => {
         margin: 10px;
       }
     }
+    .item-description {
+      color: white;
+      font-weight: bold;
+      text-align: center;
+      margin: 5px;
+      font-size: 16px;
+      @media only screen and (min-width: 650px) {
+        font-size: 18px;
+      }
+    }
+    .item-title {
+      font-weight: bold;
+      color: white;
+      text-align: center;
+      font-size: 22px;
+      @media only screen and (min-width: 650px) {
+        font-size: 32px;
+      }
+    }
   `;
   return (
     <StyledContainer>
-      {/* <Typography component="p">{props.item.title}</Typography> */}
-      {item.description && (
-        <Typography component="p">{item.description}</Typography>
-      )}
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Typography component="h2" className="item-title">
+          {item.title}
+        </Typography>
+        {item.description && (
+          <Typography
+            component="h1"
+            sx={{
+              color: "white",
+              fontWeight: "bold",
+              textAlign: "center",
+              marginTop: 5,
+              fontSize: 16,
+            }}
+            className="item-description"
+          >
+            {item.description}
+          </Typography>
+        )}
+      </Box>
 
       <Box className="button-div">
         <Button variant="contained" className="image-button callButton">
@@ -41,7 +84,9 @@ const CarouselSlide = ({ item }: CarouselSlideProps) => {
           Free Estimate
         </Button>
       </Box>
-      <Typography component="p">0% In-house Financing Available</Typography>
+      <Typography component="p" sx={{ color: "white", textAlign: "center" }}>
+        0% In-house Financing Available
+      </Typography>
     </StyledContainer>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Carousel from "react-material-ui-carousel";
 import styled from "@emotion/styled";
 import { CAROUSEL_SLIDES } from "../lib/utility/constants";
 import CarouselSlide from "../lib/components/carouselSlide";
+import { relative } from "path";
 
 const Home: NextPage = () => {
   const carouselSlides = CAROUSEL_SLIDES;
@@ -12,7 +13,21 @@ const Home: NextPage = () => {
     <div>
       <main className="main">
         <StyledContainer>
-          <Carousel>
+          <Carousel
+            autoPlay={false}
+            indicatorIconButtonProps={{
+              style: {
+                position: "relative",
+                color: "grey",
+                top: -40,
+              },
+            }}
+            activeIndicatorIconButtonProps={{
+              style: {
+                color: "white",
+              },
+            }}
+          >
             {carouselSlides.map((item, i) => (
               <CarouselSlide key={i} item={item} />
             ))}
