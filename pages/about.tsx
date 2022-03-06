e51a7983002bf3456539b9496b9633e1f565a52f
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-03-06 00:26:07 -0600

    more quick styling

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,10 +10,10 @@ const About: NextPage = () => {
     <div>
       <main>
         <PageHeader title="About Us" />
-        <Typography sx={{ textAlign: "center" }} component="h4">
+        <Typography sx={{ textAlign: "center", m: 3, fontSize: 20 }} component="h4">
           {ABOUT_TEXT.CUT_CORNER_TEXT}
         </Typography>
-        <Paper sx={{ maxWidth: "90vw", margin: "0 auto" }}>
+        <Paper sx={{ maxWidth: "90vw", margin: "0 auto", p: 2 }}>
           <Box sx={{ maxWidth: 500, margin: "0 auto" }}>
             <img
               src="images/about-us.jpg"
@@ -21,25 +21,25 @@ const About: NextPage = () => {
               alt="Picture of founder"
             />
           </Box>
-          <Paper sx={{ maxWidth: 500, margin: "0 auto" }}>
+          <Paper sx={{ maxWidth: 500, margin: "20px auto" }}>
             <Box
-              sx={{ maxWidth: 500, margin: "0 auto" }}
+              sx={{ maxWidth: 500, margin: "0 auto", p: 2 }}
               display="flex"
               flexDirection="column"
               alignItems="center"
               justifyContent="center"
             >
-              <Typography component="h4">
+              <Typography component="h4"   sx={{ m: 2}}>
                 {ABOUT_TEXT.FOUNDER_QUOTE_1}
               </Typography>
-              <Typography component="h4">
+              <Typography component="h4" sx={{ m: 2}}>
                 {ABOUT_TEXT.FOUNDER_QUOTE_2}
               </Typography>
               <Typography component="h4">Founder</Typography>
             </Box>
           </Paper>
         </Paper>
-        <Typography sx={{ textAlign: "center" }} component="h4">
+        <Typography sx={{ textAlign: "center", paddingBottom: 10, m: 2 }} component="h4">
           {ABOUT_TEXT.WORK_TEXT}
         </Typography>
       </main>
